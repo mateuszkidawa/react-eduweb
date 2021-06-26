c24753397111963a376c41755bcc36337a104bfb
Mateusz Kidawa <mateusz.kidawa@gmail.com>
2021-06-26 15:27:51 +0200

    the latest ver

--- src/App.js
+++ src/App.js
@@ -1,32 +1,26 @@
-import React, {Component} from 'react';
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends React.Component {
-  // const App = () => (
-    render(){
-      return (
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-              alt="roman"
-            >
-              Sit eos sadipscing sed accusam nonumy justo kasd erat ut duo. Tempor aliquyam ut ametnvidunt
-              accusam invidunt. Kasd ipsum clita duo stet et sanctus invidunt erat aliquyam, eirmod justo
-              vero ipsum duo sanctus clita, ipsum aliquyam sanctus est invidunt aliquyam et et clita. Et
-              amet voluptua consetetur elitr vero.kkLabore et lorem voluptua gubergren rebum gubergren sit
-              eos aliquyam eirmod, sanctus ea ut rebum et, tempor eirmod rebum dolore. Learn React
-            </a>
-          </header>
-        </div>
-// );
-      )
-    }
-}
+const App = () => (
+  <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+        alt="roman"
+      >
+        Sit eos sadipscing sed accusam nonumy justo kasd erat ut duo. Tempor aliquyam ut ametnvidunt
+        accusam invidunt. Kasd ipsum clita duo stet et sanctus invidunt erat aliquyam, eirmod justo
+        vero ipsum duo sanctus clita, ipsum aliquyam sanctus est invidunt aliquyam et et clita. Et
+        amet voluptua consetetur elitr vero.kkLabore et lorem voluptua gubergren rebum gubergren sit
+        eos aliquyam eirmod, sanctus ea ut rebum et, tempor eirmod rebum dolore. Learn React
+      </a>
+    </header>
+  </div>
+);
 
 export default App;
